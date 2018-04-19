53f6881c2f2e76052a5c64fe469b393ef37d6934
Niklas Rosenstein <rosensteinniklas@gmail.com>
2018-04-19 08:54:00 +0200

    update config files

--- config/.hyper.js
+++ config/.hyper.js
@@ -115,7 +115,11 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [],
+  plugins: [
+    "hyperlinks",
+    "hypergoogle",
+    "hyperterm-safepaste"
+  ],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
